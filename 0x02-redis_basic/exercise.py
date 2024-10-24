1af9a961de6e5bd3d1726cd1bb7d005d85c166c0
mmanyuat <mmanyuat201@gmail.com>
2024-10-24 05:29:07 +0000

    getint

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,6 +10,7 @@ import redis
 import uuid
 from typing import Union
 
+
 class Cache:
     """A class that starts redis server, takes data
     and stores it there
@@ -26,3 +27,21 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    def get(self, key: str, fn:
+            optional[callable] = None -> Union[str, bytes, int, float, None]:
+            data = self._redis.get(key)
+            if data is None:
+                return None
+
+            if fn:
+                return fn(data)
+
+            return data
+    
+    def get_str(self, key: str) -> optional[str]:
+        """Retrieve a string value from the redis"""
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> optional[int]:
+    """Retrieve an integer value from the redis"""
+        return self.get(key, lambda d: int(d))
